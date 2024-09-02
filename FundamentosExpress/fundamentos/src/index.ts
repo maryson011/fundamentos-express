c9757a9d90536ed917820b465bc286e544bb4c8e
maryson gleison <maryson011@gmail.com>
2024-09-02 09:30:16 -0300

    feat: sending different types of data

--- FundamentosExpress/fundamentos/src/index.ts
+++ FundamentosExpress/fundamentos/src/index.ts
@@ -2,8 +2,34 @@ import express from 'express'
 const app = express()
 const port = 8080
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
     res.send("<h1>Hello Word!</h1>")
+})
+
+app.get("/variavel", (req, res) => {
+    let v = "Adeus"
+    res.send(v)
+})
+
+app.get("/variavelNumerica", (req, res) => {
+    let v = 1520 // é entendido que o número é um status http
+    // res.send(v)
+    res.json(v)
+})
+
+app.get("/objeto", (req, res) => {
+    const obj = {nome: "Marcos", idade: 34}
+    res.send(obj) // obj será enviado como JSON
+})
+
+app.get("/arrayDeObjetos", (req, res) => {
+    const objs = [
+        {modelo: "Gol", ano: "2010"},
+        {modelo: "Uno", ano: "2000"},
+        {modelo: "Celta", ano: "2001"},
+    ]
+
+    res.send(objs)
 })
 
 app.listen(port, () => {
