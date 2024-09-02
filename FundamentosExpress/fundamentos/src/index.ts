59609f96ef4dc5758b409136c44c499b8b0d9b6b
maryson gleison <maryson011@gmail.com>
2024-09-02 09:52:40 -0300

    feat: sending files

--- FundamentosExpress/fundamentos/src/index.ts
+++ FundamentosExpress/fundamentos/src/index.ts
@@ -1,37 +1,46 @@
-import express from 'express'
-const app = express()
-const port = 8080
+import express from "express";
+import path from "path";
+const app = express();
+const port = 8080;
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello Word!</h1>")
-})
+  res.send("<h1>Hello Word!</h1>");
+});
 
 app.get("/variavel", (req, res) => {
-    let v = "Adeus"
-    res.send(v)
-})
+  let v = "Adeus";
+  res.send(v);
+});
 
 app.get("/variavelNumerica", (req, res) => {
-    let v = 1520 // é entendido que o número é um status http
-    // res.send(v)
-    res.json(v)
-})
+  let v = 1520; // é entendido que o número é um status http
+  // res.send(v)
+  res.json(v);
+});
 
 app.get("/objeto", (req, res) => {
-    const obj = {nome: "Marcos", idade: 34}
-    res.send(obj) // obj será enviado como JSON
-})
+  const obj = { nome: "Marcos", idade: 34 };
+  res.send(obj); // obj será enviado como JSON
+});
 
 app.get("/arrayDeObjetos", (req, res) => {
-    const objs = [
-        {modelo: "Gol", ano: "2010"},
-        {modelo: "Uno", ano: "2000"},
-        {modelo: "Celta", ano: "2001"},
-    ]
+  const objs = [
+    { modelo: "Gol", ano: "2010" },
+    { modelo: "Uno", ano: "2000" },
+    { modelo: "Celta", ano: "2001" },
+  ];
 
-    res.send(objs)
-})
+  res.send(objs);
+});
+
+app.get("/html", (req, res) => {
+  res.sendFile(path.join(__dirname, "./", "assets", "pagina.html"));
+});
+
+app.get("/pdf", (req, res) => {
+  res.sendFile(path.join(__dirname, "./", "assets", "PDF-teste.pdf"));
+});
 
 app.listen(port, () => {
-    console.log(`Server running in port ${port}!!`)
-})
+  console.log(`Server running in port ${port}!!`);
+});
